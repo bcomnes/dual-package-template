11ab1b2784accd94dda2f2d7de68e476ac5eb665
Bret Comnes <bcomnes@gmail.com>
2020-09-14 10:22:41 -0700

    chore: update to an actual working esm setup

--- main.js
+++
@@ -1,5 +0,0 @@
-// ESM syntax is supported.
-const thing = { foo: 'bar' }
-export {
-  thing
-}
--- test.js
+++
@@ -1,9 +0,0 @@
-import tape from 'tape'
-import ptape from 'tape-promise'
-import { thing } from './main'
-const test = ptape(tape)
-
-test('a test', async t => {
-  console.log(thing)
-  t.ok('pass')
-})
